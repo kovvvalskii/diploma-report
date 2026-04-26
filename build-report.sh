82836903db065c641a773fe2a882ca83f79fe615
kovvvalskii <bilkoudan@gmail.com>
2026-04-26 15:09:15 +0200

    images refactored, intro added

--- build-report.sh
+++ build-report.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-cd tex-generator &&
-./economics.py > ../report/sections/sec_economics.tex &&
-cd ../report &&
-latexmk -pdf diploma/diploma_report.tex
 
 SRC_DIR="./uml-sources"
-OUT_DIR="./../pictures"
-
-
+OUT_DIR="./report/picture/diagramm"
 docker run --rm \
   -v "$PWD/$SRC_DIR:/input" \
   -v "$PWD/$OUT_DIR:/output" \
@@ -15,5 +9,13 @@ docker run --rm \
   -tpng \
   -o "/output" \
   "/input/*.puml"
+
+cd tex-generator &&
+./economics.py > ../report/sections/sec_economics.tex &&
+
+cd ../report &&
+latexmk -pdf diploma/diploma_report.tex
+
+cd ..
 
 echo "DONE!"
